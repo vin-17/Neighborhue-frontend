773587f394e791d6654a083d81ef416936461813
deepak singh <deepaksingh101101@gmail.com>
2024-02-13 14:02:20 +0530

    solutions section title edited and images swaped

--- src/Component/Solutions/Solutions.jsx
+++ src/Component/Solutions/Solutions.jsx
@@ -38,7 +38,7 @@ const Solutions = () => {
             <div className='solutionsContainer'>
 
                 <div className="solutionIntro">
-                    <p className='boldSolutionIntro'>Lorem ipsum dolor <br /> <span className='boldSolutionIntro boldSolutionIntroBg'>Sit amet consectetur</span></p>
+                    <p className='boldSolutionIntro'>How to use the <br /> <span className='boldSolutionIntro boldSolutionIntroBg'>Hue AI model</span></p>
                     <p className='lightSolutionIntro'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium</p>
                     <p id="lightSolutionMobile">Experience our AI chatbot's instant, personalized excellence, loved by 86% of users</p>
                 </div>
@@ -46,15 +46,15 @@ const Solutions = () => {
                 <div className="solutionItems">
                     <div className="solution">
                         <img src={solution1} alt="" className='solutionImage' />
-                        <p className='alignCenter'>Advertisement 1<br />Lorem ipsum</p>
-                    </div>
-                    <div className="solution">
-                        <img src={solution2} alt="" className='solutionImage' />
-                        <p className='alignCenter'>Advertisement 1<br />Lorem ipsum</p>
+                        <p className='alignCenter'>Ask Hue for the best<br />areas in the city</p>
                     </div>
                     <div className="solution">
                         <img src={solution3} alt="" className='solutionImage' />
-                        <p className='alignCenter'>Advertisement 1<br />Lorem ipsum</p>
+                        <p className='alignCenter'>Receive recommendation and<br />Read our blogs</p>
+                    </div>
+                    <div className="solution">
+                        <img src={solution2} alt="" className='solutionImage' />
+                        <p className='alignCenter'>Your data & preferences<br />are always secured</p>
                     </div>
                 </div>
 
