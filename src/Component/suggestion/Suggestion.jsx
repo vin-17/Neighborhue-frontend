9ce594bb2c2f9b93474d33ff658d0c3de019f25a
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-14 15:46:54 +0530

    making responsive and add button effect

--- src/Component/suggestion/Suggestion.jsx
+++ src/Component/suggestion/Suggestion.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Suggestion.css';
 
 export default function Suggestion({ suggestions }) {
+  const [visibility, setVisibility] = useState(Array(suggestions.length).fill(true));
+
+  const hidePrompt = (index) => {
+    setVisibility((prevVisibility) => {
+      const updatedVisibility = [...prevVisibility];
+      updatedVisibility[index] = false;
+      return updatedVisibility;
+    });
+  };
+
   return (
     <div className='sug_outer'>
       {suggestions && suggestions.map((suggestion, index) => (
-        <div className='sug_box' key={index}>
-          <h5 style={{ color: "#1e1e1e" }}>{suggestion.suggestion}</h5>
-          <button><i class="fa-solid fa-circle-xmark" style={{ color: "#DD6745", fontSize: "x-large" }}></i></button>
-        </div>
+        visibility[index] && (
+          <div className='sug_box' key={index}>
+            <h5 style={{ color: "#1e1e1e" }}>{suggestion.suggestion}</h5>
+            <button onClick={() => hidePrompt(index)}>
+              <i className="fa-solid fa-circle-xmark" style={{ color: "#DD6745", fontSize: "x-large" }}></i>
+            </button>
+          </div>
+        )
       ))}
     </div>
   );
