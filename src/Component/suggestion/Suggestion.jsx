275b53d938a890beaf0f7036477f34d19d22f751
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-14 14:44:21 +0530

    change suggestion box color oultine

--- src/Component/suggestion/Suggestion.jsx
+++ src/Component/suggestion/Suggestion.jsx
@@ -6,7 +6,8 @@ export default function Suggestion({ suggestions }) {
     <div className='sug_outer'>
       {suggestions && suggestions.map((suggestion, index) => (
         <div className='sug_box' key={index}>
-          <h5 style={{ color: "black" }}>{suggestion.suggestion}</h5>
+          <h5 style={{ color: "#1e1e1e" }}>{suggestion.suggestion}</h5>
+          <button><i class="fa-solid fa-circle-xmark" style={{color: "#FFD43B"}}></i></button>
         </div>
       ))}
     </div>
