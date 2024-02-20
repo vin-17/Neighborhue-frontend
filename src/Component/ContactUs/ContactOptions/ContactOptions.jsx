f5b4ebcdd442dd43018f4624ca60c391f62e4afc
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-20 15:48:09 +0530

    remove unwanted contact cards

--- src/Component/ContactUs/ContactOptions/ContactOptions.jsx
+++ src/Component/ContactUs/ContactOptions/ContactOptions.jsx
@@ -17,15 +17,6 @@ const ContactOptions = () => {
             <div className="contactHolder">
                 <div className="contactCard">
                     <div className="iconHolder">
-                        <img src={phone} alt="" className="icon" />
-                        
-                    </div>
-                    <h2 className="contactCardHeading">Contact with phone</h2>
-                    <p className="contactCardInfo">Speak to our friendly Team.</p>
-                    <a href='tel:+91000XXX0000' className="contactLink">+91 000XXX0000</a>
-                </div>
-                <div className="contactCard">
-                    <div className="iconHolder">
                         <img src={chat} alt="" className="icon" />
                     </div>
                     <h2 className="contactCardHeading">Chat with us</h2>
@@ -39,14 +30,6 @@ const ContactOptions = () => {
                     <h2 className="contactCardHeading">Contact with Email</h2>
                     <p className="contactCardInfo">Support team will help you.</p>
                     <a href="mailto:support@neighborhue.com" className="contactLink">support@neighborhue.com</a>
-                </div>
-                <div className="contactCard">
-                    <div className="iconHolder">
-                        <img src={map} alt="" className="icon" />
-                    </div>
-                    <h2 className="contactCardHeading">Visit our Office</h2>
-                    <p className="contactCardInfo">Lorem Ipsum dolor sit</p>
-                    <a href="https://www.google.com/maps" target='_blank' className="contactLink">View on Goggle map</a>
                 </div>
             </div>
 
