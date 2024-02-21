67891bc52221f4ce827b8fc6c5db9a5c67474d6a
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-21 18:58:06 +0530

    comment BetaReviews for removing the section

--- src/Component/Pricing/Pricing.jsx
+++ src/Component/Pricing/Pricing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PriceOption from './PriceOption/PriceOption'
 import TrustedBy from './trustedBy/TrustedBy'
-import BetaReviews from './BetaReviews/BetaReviews'
+// import BetaReviews from './BetaReviews/BetaReviews'
 import FAQ from '../FAQ/FAQ'
 import './Pricing.css'
 
@@ -9,16 +9,16 @@ import './Pricing.css'
 
 const Pricing = () => {
     return (
-        <>  
-        
-            
-                <div className='pricingContainer' id='priceTop'>
-                    <PriceOption />
-                    <TrustedBy />
-                    <BetaReviews />
-                </div>
-            
-            
+        <>
+
+
+            <div className='pricingContainer' id='priceTop'>
+                <PriceOption />
+                <TrustedBy />
+                {/* <BetaReviews /> */}
+            </div>
+
+
             <FAQ />
         </>
 
