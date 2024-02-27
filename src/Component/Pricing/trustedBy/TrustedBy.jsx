bac47238f920bcb034811c4aa1bd656e8310697d
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-27 15:34:14 +0530

    remove customer support section

--- src/Component/Pricing/trustedBy/TrustedBy.jsx
+++ src/Component/Pricing/trustedBy/TrustedBy.jsx
@@ -14,10 +14,10 @@ import dristi from '../../../Assets/dristi.jpg'
 import iic from '../../../Assets/IIC.jpg'
 import bit from '../../../Assets/bit logo.png'
 import techavatar from '../../../Assets/techavatar.png'
-import rightArrow from '../../../Assets/arrowRight.png'
-import ceo from '../../../Assets/reviewer2.webp'
+// import rightArrow from '../../../Assets/arrowRight.png'
+// import ceo from '../../../Assets/reviewer2.webp'
 
-import customerCareBg from '../../../Assets/customerSupportVector.svg'
+// import customerCareBg from '../../../Assets/customerSupportVector.svg'
 
 
 import './TrustedBy.css'
@@ -25,50 +25,50 @@ import './TrustedBy.css'
 let cmpArr = [cmp1, cmp2, cmp3, cmp4, cmp5, cmp6, cmp7, cmp8];
 // console.log(cmpArr)
 
-const CustomerSupport = () => {
-    return (
-        <div className="customerSupportContainer">
+// const CustomerSupport = () => {
+//     return (
+//         <div className="customerSupportContainer">
 
-            <div className="supportLeft">
-                <h2 className="customerSupportHeading">Customers love our support team, and so will you?</h2>
-                <div className="customerSupportActions">
-                    <a href="#" target='_blank'>
-                        {/* <div className="scheduleDemo">
-                            Schedule a Demo
-                            <img src={rightArrow} alt="" />
-                        </div> */}
-                        <button className="comingBtn" style={{background: "linear-gradient(90deg, rgba(212,65,23,0.7988445378151261) 24%, rgba(84,24,207,0.8016456582633054) 100%)",border:"none",borderRadius:"7px",padding:"12px 18px",color:"#ffff"}}>
-                        Schedule a Demo
-                        <i class="fa-solid fa-arrow-right fa-rotate-by fa-lg" style={{rotate:"-45deg"}}></i>
-                    </button>
-                    </a>
-                    <a href="#" target='_blank' className="peopleReview">
-                        Why People love us
-                    </a>
-                </div>
-                <div className="ceoComment">
-                    <p className="ceoWords">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae"</p>
-                    <div className="ceo">
-                        <img src={ceo} alt="ceo image" />
-                        XYZ,&nbsp;<span>CEO of Neighborhue</span>
-                    </div>
-                </div>
-            </div>
+//             <div className="supportLeft">
+//                 <h2 className="customerSupportHeading">Customers love our support team, and so will you?</h2>
+//                 <div className="customerSupportActions">
+//                     <a href="#" target='_blank'>
+//                         {/* <div className="scheduleDemo">
+//                             Schedule a Demo
+//                             <img src={rightArrow} alt="" />
+//                         </div> */}
+//                         <button className="comingBtn" style={{background: "linear-gradient(90deg, rgba(212,65,23,0.7988445378151261) 24%, rgba(84,24,207,0.8016456582633054) 100%)",border:"none",borderRadius:"7px",padding:"12px 18px",color:"#ffff"}}>
+//                         Schedule a Demo
+//                         <i class="fa-solid fa-arrow-right fa-rotate-by fa-lg" style={{rotate:"-45deg"}}></i>
+//                     </button>
+//                     </a>
+//                     <a href="#" target='_blank' className="peopleReview">
+//                         Why People love us
+//                     </a>
+//                 </div>
+//                 <div className="ceoComment">
+//                     <p className="ceoWords">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae"</p>
+//                     <div className="ceo">
+//                         <img src={ceo} alt="ceo image" />
+//                         XYZ,&nbsp;<span>CEO of Neighborhue</span>
+//                     </div>
+//                 </div>
+//             </div>
 
-            <div className="supportRight">
-                {/* <div className="supportVectorContainer"> */}
-                {/* <img src={customerSupport} alt="Customer Care" className='customerSupportImg' />
-                    <img src={doodle} alt="Vector Art" className='doodleVector' />
-                    <img src={planet} alt="Vector Art" className='planetVector' />
-                    <div className="customerSupportText">Hi Lisa, is there anything i can help you today?</div> */}
+//             <div className="supportRight">
+//                 {/* <div className="supportVectorContainer"> */}
+//                 {/* <img src={customerSupport} alt="Customer Care" className='customerSupportImg' />
+//                     <img src={doodle} alt="Vector Art" className='doodleVector' />
+//                     <img src={planet} alt="Vector Art" className='planetVector' />
+//                     <div className="customerSupportText">Hi Lisa, is there anything i can help you today?</div> */}
 
-                <img src={customerCareBg} alt="" loading='lazy' />
-                {/* </div> */}
-            </div>
+//                 <img src={customerCareBg} alt="" loading='lazy' />
+//                 {/* </div> */}
+//             </div>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
 const TrustedBy = () => {
     return (
@@ -93,7 +93,7 @@ const TrustedBy = () => {
 
             </div>
 
-            <CustomerSupport />
+            {/* <CustomerSupport /> */}
         </div>
     )
 }
