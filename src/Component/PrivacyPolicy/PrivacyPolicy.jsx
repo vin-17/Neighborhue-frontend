80cfa17d65284ee84eb227b7101d0223d6f29fe3
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-28 18:16:16 +0530

    added privacy policy jsx code

---
+++ src/Component/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import "./privacy.css"
+
+const PrivacyPolicy = () => {
+    return (
+        <div className='privacy_main'>
+            <h1>PRIVACY NOTICE </h1><br />
+            <span>Last updated __________ </span><br /><br />
+            <p>This privacy notice for Neighborhue (<b>"we," "us," or "our"</b>), describes how and why we might collect, store, use, and/or share (<b>"process"</b>) your information when you use our services (<b>"Services"</b>), such as when you:<b>Questions or concerns?</b> Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services.</p>
+
+            <div className='summary'>
+                <h1>SUMMARY OF KEY POINTS </h1><br />
+
+                <p><b>This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our <span>table of contents</span> below to find the section you are looking for.</b></p><br />
+
+                <p><b>What personal information do we process?</b> When you visit, use, or navigate our Services, we may process personal information depending on how you interact with us and the Services, the choices you make, and the products and features you use. Learn more about <span>personal information you disclose to us</span>.
+                </p><br />
+
+                <p><b>Do we process any sensitive personal information?</b> We do not process sensitive personal information.</p><br />
+
+                <p><b>Do we receive any information from third parties?</b>We may receive information from public databases, marketing partners, social media platforms, and other outside sources. Learn more about information collected from other sources. </p><br />
+
+                <p><b>How do we process your information?</b> We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so. Learn more about <span>how we process your information</span>.
+                </p><br />
+
+                <p><b>In what situations and with which parties do we share personal information? </b> We may share information in specific situations and with specific third parties. Learn more about  <span>when and with whom we share your personal information</span>.
+                </p><br />
+
+                <p><b>What are your rights? </b>Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information. Learn more about <span>your privacy rights</span>.
+                </p><br />
+
+                <p><b>How do you exercise your rights?</b> The easiest way to exercise your rights is by submitting a <span>data subject access request</span>, or by contacting us. We will consider and act upon any request in accordance with applicable data protection laws.</p><br />
+
+                <p>Want to learn more about what we do with any information we collect?<br /><span>Review the privacy notice in full.</span></p>
+            </div>
+            <div className='table_of_contents'>
+                <h1>TABLE OF CONTENTS</h1>
+                <ol className='ol_contents'>
+                    <li>WHAT INFORMATION DO WE COLLECT?</li>
+                    <li>HOW DO WE PROCESS YOUR INFORMATION?</li>
+                    <li>WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?</li>
+                    <li>DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</li>
+                    <li>HOW DO WE HANDLE YOUR SOCIAL LOGINS?</li>
+                    <li>IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</li>
+                    <li>HOW LONG DO WE KEEP YOUR INFORMATION?</li>
+                    <li>DO WE COLLECT INFORMATION FROM MINORS?</li>
+                    <li>WHAT ARE YOUR PRIVACY RIGHTS?</li>
+                    <li>CONTROLS FOR DO-NOT-TRACK FEATURES</li>
+                    <li>DO WE MAKE UPDATES TO THIS NOTICE?</li>
+                    <li>HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</li>
+                    <li>HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</li>
+                </ol>
+            </div>
+            <div className='info_collection'>
+                <h1>1. WHAT INFORMATION DO WE COLLECT?</h1><br />
+                <h2>Personal information you disclose to us </h2><br />
+                <p><i><b>In Short:</b> We collect personal information that you provide to us.</i></p><br />
+
+                <p>We collect personal information that you voluntarily provide to us when you register on the Services, express an interest in obtaining information about us or our products and Services, when you participate in activities on the Services, or otherwise when you contact us. </p><br />
+
+                <p><b>Sensitive Information.</b> We do not process sensitive information.
+                    All personal information that you provide to us must be true, complete, and accurate, and
+                </p><br />
+
+                <p>you must notify us of any changes to such personal information. </p><br />
+
+                <h2>Information automatically collected </h2><br />
+                <p><i><b>In Short:</b>Some information — such as your Internet Protocol (IP) address and/or browser and device characteristics — is collected automatically when you visit our Services. </i></p><br />
+
+                <p>We automatically collect certain information when you visit, use, or navigate the Services. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our Services, and other technical information. This information is primarily needed to maintain the security and operation of our Services, and for our internal analytics and reporting purposes.</p><br />
+
+                <p>Like many businesses, we also collect information through cookies and similar technologies.
+                </p>
+
+                <div className='process_info'>
+                    <h1>2. HOW DO WE PROCESS YOUR INFORMATION? </h1><br />
+                    <p><i><b>In Short:</b> We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. </i></p><br />
+
+                    <p><b>We process your personal information for a variety of reasons, depending on how you interact with our Services, including: </b></p>
+                </div>
+
+                <div className='personal_info'>
+                    <h1>3. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION? </h1><br />
+                    <p><i><b>In Short:</b> We may share information in specific situations described in this section and/or with the following third parties. </i></p><br />
+
+                    <p>We may need to share your personal information in the following situations: </p><br />
+
+                    <p><b>Business Transfers.</b> We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company. </p><br />
+
+                    <p><b>Affiliates.</b> We may share your information with our affiliates, in which case we will require those affiliates to honor this privacy notice. Affiliates include our parent company and any subsidiaries, joint venture partners, or other companies that we control or that are under common control with us. </p><br />
+
+                    <p><b>Business Partners.</b> We may share your information with our business partners to offer you certain products, services, or promotions. </p>
+                </div>
+                <div className="cookies">
+                    <h1>4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES? </h1><br />
+                    <p><i><b>In Short:</b> We may use cookies and other tracking technologies to collect and store your information. </i></p><br />
+
+                    <p>We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Notice. </p>
+                </div>
+
+                <div className="social_logins">
+                    <h1>5. HOW DO WE HANDLE YOUR SOCIAL LOGINS? </h1><br />
+                    <p><i><b>In Short:</b> If you choose to register or log in to our Services using a social media account, we may have access to certain information about you. </i></p><br />
+
+                    <p>Our Services offer you the ability to register and log in using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, and profile picture, as well as other information you choose to make public on such a social media platform. </p><br />
+
+                    <p>We will use the information we receive only for the purposes that are described in this privacy notice or that are otherwise made clear to you on the relevant Services. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy notice to understand how they collect, use, and share your personal information, and how you can set your privacy preferences on their sites and apps. </p>
+                </div>
+                <div className="info_transfered">
+                    <h1>6. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY? </h1><br />
+                    <p><i><b>In Short:</b>We may transfer, store, and process your information in countries other than your own. </i></p><br />
+
+                    <p>Our servers are located in. If you are accessing our Services from outside, please be aware that your information may be transferred to, stored, and processed by us in our facilities and by those third parties with whom we may share your personal information (see <span>"WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?"</span> above), in and other countries. </p><br />
+
+                    <p>If you are a resident in the European Economic Area (EEA), United Kingdom (UK), or Switzerland, then these countries may not necessarily have data protection laws or other similar laws as comprehensive as those in your country. However, we will take all necessary measures to protect your personal information in accordance with this privacy notice and applicable law. </p>
+                </div>
+
+                <div className="info_keep">
+                    <h1>7. HOW LONG DO WE KEEP YOUR INFORMATION? </h1><br />
+                    <p><i><b>In Short:</b>We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice unless otherwise required by law. </i></p><br />
+
+                    <p>We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements). </p><br />
+
+                    <p>When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible. </p>
+                </div>
+
+                <div className="info_from_minors">
+                    <h1>8. DO WE COLLECT INFORMATION FROM MINORS? </h1><br />
+                    <p><i><b>In Short:</b>We do not knowingly collect data from or market to children under 18 years of age. </i></p><br />
+
+                    <p>We do not knowingly solicit data from or market to children under 18 years of age. By using the Services, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the Services. If we learn that personal information from users less than 18 years of age has been collected, we will deactivate the account and take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact.</p>
+                </div>
+
+                <div className="privacy_rights">
+                    <h1>9. WHAT ARE YOUR PRIVACY RIGHTS? </h1><br />
+                    <p><i><b>In Short:</b>You may review, change, or terminate your account at any time.</i>
+                    </p><br />
+
+                    <p><i><b>Withdrawing your consent: </b>If we are relying on your consent to process your personal information, which may be express and/or implied consent depending on the applicable law, you have the right to withdraw your consent at any time.However, please note that this will not affect the lawfulness of the processing before its withdrawal nor, when applicable law allows, will it affect the processing of your personal information conducted in reliance on lawful processing grounds other than consent.</i></p><br />
+
+                    <h2>Account Information </h2><br />
+
+                    <p>If you would at any time like to review or change the information in your account or terminate your account, you can:Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our legal terms and/or comply with applicable legal requirements.
+                    </p>
+                </div>
+
+                <div className="dont_track">
+                    <h1>10. CONTROLS FOR DO-NOT-TRACK FEATURES </h1><br />
+                    <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice. </p><br />
+                </div>
+
+                <div className="update_to_notice">
+                    <h1>11. DO WE MAKE UPDATES TO THIS NOTICE? </h1><br />
+                    <p><i><b>In Short:</b>Yes, we will update this notice as necessary to stay compliant with relevant laws. </i>
+                    </p><br />
+                    <p>We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information. </p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default PrivacyPolicy
