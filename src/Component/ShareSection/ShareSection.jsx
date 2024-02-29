a66bdfa6fa9579c348cdcc7bed79a01c341ce908
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-29 18:48:58 +0530

    remove Link import

--- src/Component/ShareSection/ShareSection.jsx
+++ src/Component/ShareSection/ShareSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Link } from 'react-router-dom';
 import "./shareSection.css"
 
 const ShareSection = () => {
