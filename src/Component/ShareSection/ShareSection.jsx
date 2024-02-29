a41fc78fb48e811154471b1666ea0e04f4784bec
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-02-29 17:39:40 +0530

    added share functionality

--- src/Component/ShareSection/ShareSection.jsx
+++ src/Component/ShareSection/ShareSection.jsx
@@ -50,14 +50,25 @@ const ShareSection = () => {
             theme: "light",
         });
     }
+    const sharebtn=async()=>{
+        let weburl = window.location.href;
+        try {
+            await navigator.share({
+              text: 'Follow me to get more web development content.',
+              url: weburl
+            })
+          } catch (error) {
+            console.log('Sharing failed!', error)
+          }
+      }
     return (
         <div className="shareSectionContainer">
             <h2 className="shareHeader"> Show You <span className="deepBlue">Care </span> Share <span className="deepBlue">Neighborhue</span></h2>
             <p className="shareData">Help the neighborhue community grow by sharing the website link and showing support via our socials</p>
 
             <div className="shareActionContainer">
-                <Link to="https://www.instagram.com/neighborhue_ai?igsh=MWFuN2M3cDlqNjE2eg%3D%3D&utm_source=qr" target='_blank'>
-                    <button className="comingBtn" style={{ background: "linear-gradient(90deg, rgba(212,65,23,0.7988445378151261) 24%, rgba(84,24,207,0.8016456582633054) 100%)", border: "none", borderRadius: "7px", padding: "12px 18px", color: "#ffff", fontSize: "clamp(12px, 2vw, 18px)" }}>
+                <Link to="" target='_blank'>
+                    <button onClick={sharebtn} className="comingBtn" style={{ background: "linear-gradient(90deg, rgba(212,65,23,0.7988445378151261) 24%, rgba(84,24,207,0.8016456582633054) 100%)", border: "none", borderRadius: "7px", padding: "12px 18px", color: "#ffff", fontSize: "clamp(12px, 2vw, 18px)" }}>
                         Share Link
                         <i class="fa-solid fa-share-nodes fa-lg" style={{ color: "#ffffff" }}></i>
                     </button>
